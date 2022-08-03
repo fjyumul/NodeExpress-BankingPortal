b090671cc27df245972d198278a1fb5c07350615
Francis Jay Yumul <francis_jay_yumul@manulife.ca>
2022-08-03 08:52:06 -0400

    commit

--- src/app.js
+++ src/app.js
@@ -9,6 +9,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req, res) => res.render('index', { title: 'Index'}));
+const accountData = fs.readFileSync('src/json/accounts.json','utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json','utf8');
+const users = JSON.parse(userData);
+
+app.get('/',(req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+app.get('/savings',(req, res) => res.render('summary', { title: 'Savings Account Summary', account: accounts.savings }));
+app.get('/checking',(req, res) => res.render('summary', { title: 'Checking Account Summary', account: accounts.checking }));
+app.get('/credit',(req, res) => res.render('summary', { title: 'Credit Account Summary', account: accounts.credit }));
+app.get('/profile',(req, res) => res.render('profile', { title: 'Profile', user: users[0] }));
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));

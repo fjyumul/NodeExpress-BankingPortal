6dc37aa76b4e981cf5af12a6659343bd7c6713fe
Francis Jay Yumul <francis_jay_yumul@manulife.ca>
2022-08-04 10:14:20 -0400

    Commit for exercise 5

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 
@@ -9,12 +10,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
-
-const accountData = fs.readFileSync('src/json/accounts.json','utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json','utf8');
-const users = JSON.parse(userData);
 
 app.get('/',(req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 app.get('/savings',(req, res) => res.render('account', { title: 'Savings Account Summary', account: accounts.savings }));
@@ -28,8 +23,7 @@ app.post('/transfer', (req, res) => {
     accounts[from].balance = parseInt(accounts[from].balance, 10) - parseInt(amount, 10);
     accounts[to].balance = parseInt(accounts[to].balance, 10) + parseInt(amount, 10);
     
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', { message: 'Transfer Completed' });
 
 });
@@ -39,8 +33,8 @@ app.post('/payment', (req, res) => {
     accounts['credit'].balance = parseInt(accounts['credit'].balance, 10) - parseInt(amount, 10);
     accounts['credit'].available = parseInt(accounts['credit'].available, 10) + parseInt(amount, 10);
     
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
+    
     res.render('payment', { message: 'Payment Successful', account: accounts.credit });
 });
 
